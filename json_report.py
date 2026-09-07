0f23a58e3a72895afa701d6b44c27c2566c58794
Werner Fouché <werner.fouche@gmail.com>
2026-09-07 18:29:08 +0200

    Update benchmark config

--- json_report.py
+++ json_report.py
@@ -7,10 +7,13 @@ jb = json.load(fileObj, object_pairs_hook=OrderedDict)
 
 def report(name):
     print name
-    print "  ", jb["benchmarks"][name]["actions"]["summary"]["aps"], "aps"
+    print "  ", jb["benchmarks"][name]["actions"]["summary"]["avg_aps"], "avg_aps"
     print "  ", jb["benchmarks"][name]["actions"]["summary"]["aps_target_rate"], "aps_target_rate"
+    print "  ", jb["benchmarks"][name]["actions"]["summary"]["avg_rt"], "avg_rt"
+    print "  ", jb["benchmarks"][name]["actions"]["summary"]["p99_rt"], "p99_rt"
+    print "  ", jb["benchmarks"][name]["actions"]["summary"]["max_rt"], "max_rt"
+    print "  ", jb["benchmarks"][name]["actions"]["summary"]["CPU_T"], "CPU_T"
+    print "  ", jb["benchmarks"][name]["actions"]["summary"]["CPU"], "CPU"
 
-report("REST1")
-report("REST2")
-report("REST3")
-report("REST3.max")
+report("Jactl")
+report("Groovy")
--- run_bench.sh
+++ run_bench.sh
@@ -6,6 +6,6 @@ export JBANG_APP_JAVA_OPTIONS="-Xmx2g -Xms2g"
 jbang run io/tulip/App.java
 echo ""
 #w3m -dump -cols 205 benchmark_report.html
-lynx -dump -width 205 benchmark_report.html
+#lynx -dump -width 205 benchmark_report.html
 #jbang run asciidoctorj@asciidoctor benchmark_config.adoc
 #jbang export fatjar io/tulip/App.java
