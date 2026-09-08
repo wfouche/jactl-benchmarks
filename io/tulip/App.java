997789276c054c6f964b1f870a9db560bff820ad
Werner Fouché <werner.fouche@gmail.com>
2026-09-08 21:15:52 +0200

    Use Tulip 2.3.6

--- io/tulip/App.java
+++ io/tulip/App.java
@@ -1,12 +1,12 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.github.tulipltt:tulip-runtime:2.3.5
+//DEPS io.github.tulipltt:tulip-runtime:2.3.6
 //SOURCES JactlUser.java
 //JAVA 26
 //FILES ../../benchmark_config.json
 //FILES ../../logback.xml
-// RUNTIME_OPTIONS -XX:+IgnoreUnrecognizedVMOptions
-// RUNTIME_OPTIONS --enable-native-access=ALL-UNNAMED
-// RUNTIME_OPTIONS --sun-misc-unsafe-memory-access=allow
+//RUNTIME_OPTIONS -XX:+IgnoreUnrecognizedVMOptions
+//RUNTIME_OPTIONS --enable-native-access=ALL-UNNAMED
+//RUNTIME_OPTIONS --sun-misc-unsafe-memory-access=allow
 //DEPS io.jactl:jactl:2.9.2
 //DEPS org.apache.groovy:groovy:5.1.2
 //DEPS com.ezylang:EvalEx:3.7.0
